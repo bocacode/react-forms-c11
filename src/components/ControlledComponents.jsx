3ea020d53f5412eca2fa3f0f7f65ae3eb29f9c8a
Todd H. Albert <todd@bocacode.com>
2023-06-30 10:10:56 -0400

    forms 2 ways

---
+++ src/components/ControlledComponents.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+const person = {
+  firstName: 'Fred',
+  lastName: 'Flinstone'
+}
+
+export default function ControlledComponents() {
+  const [firstName, setFirstName] = useState(person.firstName || '')
+  const [lastName, setLastName] = useState(person.lastName || '')
+  return (
+    <>
+      <h2>Controlled Components</h2>
+      <p>Form fields in HTML typically manage their own state.</p>
+      <p>In a <strong>controlled component</strong> we let the
+        React component take control of that state
+        using <code>value</code> and <code>onChange</code>.</p>
+      <h3>You typed: {firstName} {lastName}</h3>
+      <label htmlFor="firstName">First Name:</label>
+      <input name="firstName" type="text" value={firstName}
+        onChange={e => setFirstName(e.target.value)} />
+      <label htmlFor="firstName">Last Name:</label>
+      <input name="lastName" type="text" value={lastName}
+        onChange={e => setLastName(e.target.value)} />
+    </>
+  )
+}
